b5d36dcfe2034d37666c0c1905117e5afc53f1f2
Dan <32258443+dmspils@users.noreply.github.com>
2018-09-15 21:21:41 +0100

    Update CSP.js

--- CSP.js
+++ CSP.js
@@ -1,6 +1,10 @@
-'use strict'; exports.handler = (event, context, callback) => {
+'use strict';
+
+exports.handler = (event, context, callback) => {
+
     const response = event.Records[0].cf.response;
     const headers = response.headers;
+
     response.headers['Strict-Transport-Security'] = [{
         key: 'Strict-Transport-Security',
         value: 'max-age=63072000; includeSubDomains; preload',
@@ -29,14 +33,21 @@
         key: 'Referrer-Policy',
         value: 'no-referrer-when-downgrade',
     }];
-    response.headers['Content-Security-Policy'] = [{
-        key: 'Content-Security-Policy',
-        value: 'default-src \'self\'; script-src \'self\' ichef.bbci.co.uk www.google-analytics.com 
-\'sha256-3iegr7lvng9bwsIl8sY3sSmTbegW221KcxCn873OvB0=\'; font-src \'self\' https://fonts.googleapis.com 
-https://fonts.gstatic.com; img-src \'self\' data: https://www.google-analytics.com; style-src \'self\' 
-\'unsafe-inline\' https://fonts.googleapis.com https://fonts.gstatic.com; object-src \'none\'; report-uri 
-https://spilsbury.report-uri.io/r/default/csp/enforce; block-all-mixed-content; reflected-xss block; 
-frame-ancestors \'none\'; require-sri-for script; base-uri https://daniel.spilsbury.io; upgrade-insecure-requests',
+    response.headers['Content-Security-Policy-Report-Only'] = [{
+        key: 'Content-Security-Policy-Report-Only',
+        value: `'default-src \'self\';
+                script-src \'self\';
+                font-src \'self\' https://fonts.googleapis.com https://fonts.gstatic.com;
+                img-src \'self\' data: https://www.google-analytics.com;
+                style-src \'self\' https://fonts.googleapis.com https://fonts.gstatic.com;
+                object-src \'none\';
+                report-uri https://spilsbury.report-uri.io/r/default/csp/reportOnly;
+                block-all-mixed-content;
+                frame-ancestors \'none\';
+                require-sri-for script;
+                base-uri https://daniel.spilsbury.io;
+                upgrade-insecure-requests',`
     }];
     callback(null, response);
+
 };
