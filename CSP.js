4ef1f4fb22616222d68a6dca0f7b07ed07614fb3
Dan <32258443+dmspils@users.noreply.github.com>
2019-07-05 21:37:38 +0100

    Update CSP.js

--- CSP.js
+++ CSP.js
@@ -35,11 +35,11 @@ exports.handler = (event, context, callback) => {
     }];
     response.headers['Content-Security-Policy'] = [{
         key: 'Content-Security-Policy',
-        value: 'default-src \'self\'; script-src \'self\' https://ajax.googleapis.com/; style-src \'self\' https://fonts.googleapis.com \'unsafe-inline\'; img-src \'self\'; object-src \'none\'; font-src \'self\' https://fonts.gstatic.com; frame-ancestors \'none\'; block-all-mixed-content; base-uri https://daniel.spilsbury.io; upgrade-insecure-requests; report-uri https://europe-west1-bbc-otg-traf-mgr-bq-prod-4591.cloudfunctions.net/report-endpoint',
+        value: 'default-src \'self\'; script-src \'self\' https://ajax.googleapis.com/; style-src \'self\' https://fonts.googleapis.com \'unsafe-inline\'; img-src \'self\'; object-src \'none\'; font-src \'self\' https://fonts.gstatic.com; frame-ancestors \'none\'; block-all-mixed-content; base-uri https://daniel.spilsbury.io; upgrade-insecure-requests; report-uri https://europe-west1-bbc-otg-traf-mgr-bq-prod-4591.cloudfunctions.net/report-endpoint; report-uri https://spilsbury.report-uri.com/r/d/csp/enforce',
     }]; 
     response.headers['Content-Security-Policy-Report-Only'] = [{
         key: 'Content-Security-Policy-Report-Only',
-        value: 'default-src \'self\'; script-src \'self\' https://ajax.googleapis.com/; style-src \'self\' https://fonts.googleapis.com \'unsafe-inline\'; img-src \'self\'; object-src \'none\'; font-src \'self\' https://fonts.gstatic.com; block-all-mixed-content; base-uri https://daniel.spilsbury.io; report-to csp-endpoint',
+        value: 'default-src \'self\'; script-src \'self\' https://ajax.googleapis.com/; style-src \'self\' https://fonts.googleapis.com \'unsafe-inline\'; img-src \'self\'; object-src \'none\'; font-src \'self\' https://fonts.gstatic.com; block-all-mixed-content; base-uri https://daniel.spilsbury.io; report-to scott; report-to bbc',
  
     }];
     response.headers['X-Xss-Test'] = [{
@@ -48,11 +48,15 @@ exports.handler = (event, context, callback) => {
     }];   
     response.headers['Report-To'] = [{
         key: 'Report-To',
-        value: '{\"group\": \"csp-endpoint\", \"max_age\": 3600, \"endpoints\": [{\"url\": \"https://spilsbury.report-uri.com/a/d/g"}], \"include_subdomains\": true}',
+        value: '{\"group\": \"scott\", \"max_age\": 3600, \"endpoints\": [{\"url\": \"https://spilsbury.report-uri.com/a/d/g"}], \"include_subdomains\": true}',
+    }];
+    response.headers['Report-To'] = [{
+        key: 'Report-To',
+        value: '{\"group\": \"bbc\", \"max_age\": 3600, \"endpoints\": [{\"url\": \"https://europe-west1-bbc-otg-traf-mgr-bq-prod-4591.cloudfunctions.net/report-endpoint"}], \"include_subdomains\": true}',
     }];
     response.headers['NEL'] = [{
         key: 'NEL',
-        value: '{\"group\": \"csp-endpoint\", \"max_age\": 3600, \"include_subdomains\": true}',
+        value: '{\"group\": \"scott\", \"max_age\": 3600, \"include_subdomains\": true}',
     }];      
     callback(null, response);
 
